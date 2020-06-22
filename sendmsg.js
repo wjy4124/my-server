4da9eea5c14fafbabedb566d407b2da1c49f85c0
weijy <wjy4124@qq.com>
2020-06-22 22:56:10 +0800

    event

--- sendmsg.js
+++ sendmsg.js
@@ -1,4 +1,6 @@
+const process = require('process');
 const ChatBot = require('dingtalk-robot-sender');
+console.info('DINGTALK_ACCESSTOKEN', process.env.DINGTALK_ACCESSTOKEN)
 const robot = new ChatBot({
   baseUrl: 'https://oapi.dingtalk.com/robot/send',
   accessToken: process.env.DINGTALK_ACCESSTOKEN,
